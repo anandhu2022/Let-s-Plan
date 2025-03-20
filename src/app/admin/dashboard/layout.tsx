cac97e986876c32e978a5a45fc488fff117479c4
anandhu2022 <anandhu2022@gmail.com>
2025-03-21 02:03:27 +0530

    Admin dashboard layout files

---
+++ src/app/admin/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from "react";
+
+const Page = ({children}:{children:ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default Page;
